b1f648257dd489965eaacc4ffa3763fe91a897b0
Juston Hart <contact@justonh.art>
2024-06-20 12:47:58 -0500

    Make all.js actually recursive

--- all.js
+++ all.js
@@ -1,11 +1,12 @@
 /**
- * Goal: Write a function called all which accepts an array and a callback and returns true if every value in the array returns true when passed as parameter to the callback function
+ * Goal: Write a recursive function called all which accepts an array and a callback and returns true if every value in the array returns true when passed as parameter to the callback function
  */
 function all(arr, callback){
-	return arr.reduce((current, next) => current && callback(next), true);
+	if(!arr.length) return true;
+	return callback(arr[0]) && all(arr.slice(1, arr.length), callback); 
 }
 
-const arr = [1,2,3];
+const arr = [1, 2, 3];
 console.log(`arr: ${arr}`);
 
 function isEven(number) {
